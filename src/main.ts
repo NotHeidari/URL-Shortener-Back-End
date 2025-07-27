d742b3a30252c21a09157fe8a4e43156b5497f5c
MohammadAli Heidary <mohammad8383ali@gmail.com>
2025-07-27 22:24:07 +0330

    add env files and config dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// Load environment variables based on NODE_ENV
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+const envFile = process.env.NODE_ENV === 'production' ? '.production.env' : '.development.env';
+dotenv.config({ path: path.resolve(__dirname, '..', envFile) });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
